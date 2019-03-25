585cee8193a48e1cfe706f3bca83f282fb731875
Sean Coleman <scoleman2296@gmail.com>
2019-03-24 20:00:36 -0400

    Updated Footer

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ function toggleNav() {
     nav.style.height = '60px';
     menu.style.visibility = 'hidden';
 
-    let arrayLength = navItems.length;
     for (var i = 0; i < navItems.length; i++) {
       navItems[i].style.opacity = 0;
     }
@@ -35,17 +34,29 @@ let toggleInfoStatus = false;
 
 function toggleInfo() {
   let infoBlock = document.querySelector('.info-block');
+  let infoBlockItems = document.querySelectorAll('info-block ul li');
   let infoBlockUl = document.querySelector('.info-block ul');
+  let infoBlockBtn = document.querySelector('.info-block-btn');
 
   if (toggleInfoStatus === false) {
-    infoBlock.style.height = '160px';
+    infoBlock.style.height = '170px';
     infoBlockUl.style.visibility = 'visible';
+    infoBlockBtn.style.transform = 'rotate(180deg)';
+
+    // for (let i = 0; i < infoBlockItems.length; i++) {
+    //   infoBlockItems[i].style.opacity = 1;
+    // }
 
     toggleInfoStatus = true;
 
   } else if (toggleInfoStatus === true) {
-    infoBlock.style.height = '20px';
+    infoBlock.style.height = '40px';
     infoBlockUl.style.visibility = 'hidden';
+    infoBlockBtn.style.transform = 'rotate(0deg)';
+
+    // for (let i = 0; i < infoBlockItems.length; i++) {
+    //   infoBlockItems[i].style.opacity = 1;
+    // }
 
     toggleInfoStatus = false;
   }
